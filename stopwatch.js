df19b06c2fead96b8a8280d971e0e7d6a51eb38b
SarayuYalagandula <168672371+SarayuYalagandula@users.noreply.github.com>
2024-05-16 13:21:56 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,50 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 0;
+let secs = 0;
+let mins = 0;
+let h=0
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00 : 00`;
+    msec = secs = mins = h = 0;
+    laptime=[];
+    laplist.innerHTML='';
+});
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+            mins++;
+            if(mins == 60){
+                mins=0;
+                h++;
+            }
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    let hString = h < 10 ? `0${h}` : h;
+    timerDisplay.innerHTML = `${hString} : ${minsString} : ${secsString} : ${msecString}`;
+}
